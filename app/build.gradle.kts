3f89b929ff60c94af0c5fc229fe711c70fb4c5db
teepakorn Kumvong <teeprakorn.toey@gmail.com>
2024-08-01 17:10:23 +0700

    First Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "th.ac.rmutto.duangdee"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "th.ac.rmutto.duangdee"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/th/ac/rmutto/duangdee/LoginActivity.kt
@@ -0,0 +1,92 @@
+package th.ac.rmutto.duangdee
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.StrictMode
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import org.json.JSONObject
+import th.ac.rmutto.duangdee.MainActivity
+import th.ac.rmutto.duangdee.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //For an synchronous task
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
+        val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
+        val buttonLogin = findViewById<Button>(R.id.ButtonLogin)
+        val buttonRegister = findViewById<Button>(R.id.RegisterButton)
+
+        buttonLogin.setOnClickListener {
+            val username = editTextUsername.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (username.isEmpty()) {
+                editTextUsername.error = "Username is required"
+                return@setOnClickListener
+            }
+            else if (password.isEmpty()) {
+                editTextPassword.error = "Password is required"
+                return@setOnClickListener
+            }
+
+            val url = "http://10.13.4.231:3000/api/login"
+            val okHttpClient = OkHttpClient()
+            val formBody: RequestBody = FormBody.Builder()
+                .add("username",username)
+                .add("password",password)
+                .build()
+            val request: Request = Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build()
+
+            val response = okHttpClient.newCall(request).execute()
+
+            if(response.isSuccessful){
+                val obj = JSONObject(response.body!!.string())
+                val status = obj["status"].toString()
+
+                if (status == "true") {
+                    //redirect to main page
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    val message = obj["message"].toString()
+                    Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+                }
+
+            }else{
+                Toast.makeText(applicationContext, "ไม่สามารถเชื่อต่อกับเซิร์ฟเวอร์ได้", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        buttonRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
