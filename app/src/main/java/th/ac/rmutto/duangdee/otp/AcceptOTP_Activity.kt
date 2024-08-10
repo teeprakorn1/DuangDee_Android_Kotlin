86580c2699ccf1e217fce58c6ae4b9ba95d451fb
Suttipong <suttipong22not@gmail.com>
2024-08-10 19:54:48 +0700

    VERSION 0.4.6

---
+++ app/src/main/java/th/ac/rmutto/duangdee/otp/AcceptOTP_Activity.kt
@@ -0,0 +1,21 @@
+package th.ac.rmutto.duangdee.otp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import th.ac.rmutto.duangdee.R
+
+class AcceptOTP_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_accept_otp)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
